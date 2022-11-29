16e99734f26d43d4d7c950b90dbbc178aeb405e2
Shubhranshu Singh <shubhranshu.singh@alumni.iitgn.ac.in>
2022-11-29 00:31:55 -0500

    Refactor; Speed up lookup using KD Tree

--- src/build_OD.py
+++ src/build_OD.py
@@ -1,11 +1,6 @@
 # %%
 import numpy as np
 import pandas as pd
-import glob
-import os.path
-import datetime
-import os
-import data_preprocess
 import build_grid
 # df = data_preprocess.read_all_users('C:\\CMU\\18898_Graph\\Mobility_GSP\\Data')
 # df.to_pickle('beijing_geolife_OD.pkl')
@@ -13,11 +8,8 @@ import build_grid
 # %%
 df_beijing = pd.read_pickle('..\\beijing_geolife_OD.pkl')
 
-grid = build_grid.bulid_grid_cells()
-xlat, xlon = grid.beijing_grid_cell_centers()
-x, y =np.meshgrid(xlat, xlon)
-grid_lat_lon = np.array((x.ravel(), y.ravel())).T
-cells = grid.find_closest_cell(grid_lat_lon, df_beijing[['lat', 'lon']].to_numpy())
+grid = build_grid.bulid_grid_cells(n=1000, m=1000)
+cells = grid.find_closest_cell(df_beijing[['lat', 'lon']].to_numpy())
 edges = []
 for i in range(0, len(cells), 2):
     if cells[i] != cells[i+1]: # No self loops
--- src/build_grid.py
+++ src/build_grid.py
@@ -1,33 +1,24 @@
 import numpy as np
-import pandas as pd
+from scipy.spatial import KDTree
+
 class bulid_grid_cells():
-    # def __init__(self) -> None:
-    #     pass
-    def __init__(self):
-        self.latInit = 39.4396
-        self.lonInit = 115.4238
-        self.latFinal = 41.06083
-        self.lonFinal = 117.50671
-        # return self.latInit, self.lonInit, self.latFinal, self.lonFinal
+    def __init__(self, latInit = 39.4396, lonInit = 115.4238, latFinal = 41.06083, lonFinal = 117.50671, n=10, m=10):
+        self.latInit = latInit
+        self.lonInit = lonInit
+        self.latFinal = latFinal
+        self.lonFinal = lonFinal
+        self.n = n
+        self.m = m
+        self.grid = self.create_2d_grid()
+        self.lookup_tree = KDTree(self.grid)
+
     def beijing_grid_cell_centers(self):
-        latInit = self.latInit
-        lonInit = self.lonInit
-        latFinal = self.latFinal
-        lonFinal = self.lonFinal
-        n = 28
-        m = 36
-        Xlat = np.zeros(n)
-        Xlon = np.zeros(m)
-        deltaLat = latFinal - latInit
-        deltaLon = lonFinal - lonInit
-        Xlat[0] = latInit + deltaLat/(2*n)
-        Xlat[-1] = latFinal - deltaLat/(2*n)
-        Xlon[0] = lonInit + deltaLon/(2*m)
-        Xlon[-1] = lonFinal - deltaLon/(2*m)
-        for i in range(1, len(Xlat)-1):
-            Xlat[i] = Xlat[i-1]+deltaLat/n
-        for i in range(1, len(Xlon)-1):
-            Xlon[i] = Xlon[i-1]+deltaLon/n
+        deltaLat = self.latFinal - self.latInit
+        deltaLon = self.lonFinal - self.lonInit
+
+        Xlat = np.linspace(self.latInit + deltaLat/(2*self.n), self.latFinal - deltaLat/(2*self.n), self.n) 
+        Xlon = np.linspace(self.lonInit + deltaLon/(2*self.m), self.lonFinal - deltaLon/(2*self.m), self.m)
+
         return Xlat, Xlon
 
     def coordinates_inside_beijing(self, df):
@@ -35,13 +26,11 @@ class bulid_grid_cells():
         df_bejing = df[mask_in_beijing]
         return df_bejing
 
-    def find_closest_cell(self, grid, points):
-        # grid (N, 2)
-        # points (M, 2)
+    def create_2d_grid(self):
+        xlat, xlon = self.beijing_grid_cell_centers()
+        x, y = np.meshgrid(xlat, xlon)
+        return np.array((x.ravel(), y.ravel())).T
 
-        grid = np.expand_dims(grid, axis=1) # (N, 1, 2)
-        points = np.expand_dims(points, axis=0) # (1, M, 2)
-        dists = np.sum((grid - points)**2, axis = 2) # L2 dist (N, M)
-
-        return dists.argmin(axis = 0) #(M,)
+    def find_closest_cell(self, points):
+        return self.lookup_tree.query(points)[1]
 
